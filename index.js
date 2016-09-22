71b01c38b32b28e5cc26acb8eb389fe3d32becfe
jwelker17 <jwelker9@gmail.com>
2016-09-22 00:38:39 -0600

    Getting closer

---
+++ index.js
@@ -0,0 +1,26 @@
+// $('#false-image').on('load', function(){
+//   console.log('yes');
+//   $('#image-container').css('background-image', 'url(defy_creations_landing_page.jpg)')
+// });
+$(function(){
+  function submit(name, email, callback){
+    console.log('working');
+    var url = 'https://script.google.com/macros/s/AKfycbyzpCsDOZ9pcIHYvtCQ68xBTxcHIZyPQa7kY6qxyVz04L992cCv/exec';
+    var script = document.createElement('SCRIPT');
+    script.src = url + '?name=' + name + '&email=' + email;
+    script.type = 'application/javascript';
+    script.onload = function(){
+      script.remove();
+      typeof callback === 'function' && callback();
+    }
+    document.body.appendChild(script);
+  }
+
+
+  $('#submit-button').click(function(){
+    var email = $('#email-input').val()
+      submit('', email)
+  })
+
+
+});
